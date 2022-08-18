a191eb457e25075f3cfeed14561d350384280725
pedro-drosa <pedro_drosa@hotmail.com>
2022-08-18 17:39:52 -0300

    feat(ListAllUsersController.ts): add exception handling

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
-    const users = this.listAllUsersUseCase.execute({
-      user_id: String(user_id),
-    });
-    return response.json(users);
+    try {
+      const { user_id } = request.headers;
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
